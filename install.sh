c8aa33fcd53e965b48a83485eba32e04d72c9d7a
Mike Flynn <spikeon@gmail.com>
2017-06-06 14:45:15 -0700

    Perhaps this one will work better?

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
 cp ./99-am-i-ugly /etc/update-motd.d
 
 cp ./.zshrc ~/.zshrc
