23c0d0eadf0cda71e2c876a38f7e98d3185aa487
Mike Flynn <spikeon@gmail.com>
2017-06-06 15:45:12 -0700

    It works, but throws some errors on the last step...

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~/.ohmy
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+rm -rf /etc/update-motd.d/99-one-click
 cp ./99-am-i-ugly /etc/update-motd.d
 cp ./.zshrc ~/.zshrc
 chsh -s $(which zsh)
